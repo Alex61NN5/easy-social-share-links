895a8b5446af7eaf4f46e6309dcbddacef2fa9b3
Alex Ginns <alexginns@gmail.com>
2019-08-06 11:13:42 +1000

    fix: email share bug

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function shareLink(provider, link, text = null, emailBody = null) {
 			return 'https://www.linkedin.com/shareArticle?mini=true&url=' + link + (text ? '&summary=' + text : '');
 		case 'em':
 			// text is the subject and emailBody is the text bfore the link
-			return 'mailto:' + (text ? '?subject=' + text : '') + '&body=' + (emailBody ? emailBody : '') + ' ' + link;
+			return 'mailto:' + (text ? '?subject=' + text : '') + (text ? '&body=' : '?body=') + (emailBody ? emailBody : '') + ' ' + link;
 		default:
 			return '#';
 	}
